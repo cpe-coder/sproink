ed1ee8205663351aa68ced8679cecbaf16cacb2f
Grian Gajila <griangajila2@gmail.com>
2025-08-20 11:51:17 +0800

    apply nativewind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+	return (
+		<View className="bg-white">
+			<Text className="text-blue-600">
+				Edit app/index.tsx to edit this screen.
+			</Text>
+		</View>
+	);
+}
