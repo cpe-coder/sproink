b1e16c82b585c279ade00bd4b4076fe77de93d80
Grian Gajila <griangajila2@gmail.com>
2025-08-21 12:03:51 +0800

    update icon

--- constant/icon.ts
+++ constant/icon.ts
@@ -1,6 +1,7 @@
 import eyeClose from "@/assets/icon/eye-hide.png";
 import eye from "@/assets/icon/eye.png";
 import home from "@/assets/icon/home.png";
+import logo from "@/assets/icon/logo.png";
 import settings from "@/assets/icon/setting.png";
 import user from "@/assets/icon/user.png";
 
@@ -10,4 +11,5 @@ export const icon = {
 	user,
 	home,
 	settings,
+	logo,
 };
