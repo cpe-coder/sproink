0486c6f49efcd052e366b73e69a6ff0d91edcba1
Grian Gajila <griangajila2@gmail.com>
2025-08-28 18:49:42 +0800

    wip

--- components/contols.tsx
+++ components/contols.tsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import { TimerPickerModal } from "react-native-timer-picker";
 
 const Controls = () => {
+	const [showPicker, setShowPicker] = useState(false);
+	const [alarmString, setAlarmString] = useState<string | null>(null);
+
+	const formatTime = ({
+		hours,
+		minutes,
+		seconds,
+	}: {
+		hours?: number;
+		minutes?: number;
+		seconds?: number;
+	}) => {
+		const timeParts = [];
+
+		if (hours !== undefined) {
+			timeParts.push(hours.toString().padStart(2, "0"));
+		}
+		if (minutes !== undefined) {
+			timeParts.push(minutes.toString().padStart(2, "0"));
+		}
+		if (seconds !== undefined) {
+			timeParts.push(seconds.toString().padStart(2, "0"));
+		}
+
+		return timeParts.join(":");
+	};
+
 	return (
 		<View className="p-2">
 			<Text className="font-medium text-slate-700">Controls</Text>
+			<View className=" py-2 w-28 items-center rounded-lg">
+				<TouchableOpacity
+					activeOpacity={0.7}
+					onPress={() => setShowPicker(true)}
+				>
+					<View style={{ alignItems: "center" }}>
+						{alarmString !== null ? (
+							<Text className="text-slate-700 font-medium">{alarmString}</Text>
+						) : null}
+						<TouchableOpacity
+							activeOpacity={0.7}
+							onPress={() => setShowPicker(true)}
+						>
+							<View className="mt-3">
+								<Text className="text-slate-700 font-medium">Set Alarm 🔔</Text>
+							</View>
+						</TouchableOpacity>
+					</View>
+				</TouchableOpacity>
+				<TimerPickerModal
+					visible={showPicker}
+					setIsVisible={setShowPicker}
+					onConfirm={(pickedDuration) => {
+						setAlarmString(formatTime(pickedDuration));
+						setShowPicker(false);
+					}}
+					modalTitle="Set Alarm"
+					onCancel={() => setShowPicker(false)}
+					closeOnOverlayPress
+					use12HourPicker
+				/>
+			</View>
 			<View className="flex flex-row justify-around w-full px-4 bg-slate-300 py-5 rounded-md mb-6">
 				<TouchableOpacity className="bg-slate-700 py-2 w-28 items-center rounded-lg">
 					<Text className="text-slate-50 font-medium">Water</Text>
