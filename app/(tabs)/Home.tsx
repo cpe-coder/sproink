ab6c7ee40c391e301a4bd79e5217d86c4e90ca54
Grian Gajila <griangajila2@gmail.com>
2025-08-29 12:38:39 +0800

    wip

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,14 +1,16 @@
 import { Controls } from "@/components";
-import { icon } from "@/constant/icon";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const Home = () => {
 	return (
-		<View className="flex-1 items-center py-8">
-			<Text className="text-3xl font-bold text-slate-700">Welcome Back!</Text>
-			<Image className="w-40 h-40" source={icon.logo} />
-
+		<View className="flex-1 p-4">
+			<Text className="text-2xl text-left font-bold text-slate-500">
+				Welcome Back!
+			</Text>
+			<Text className="text-base font-medium text-slate-400 mb-6">
+				The sproink makes your job easier.
+			</Text>
 			<View className="flex-1">
 				<Controls />
 			</View>
--- components/contols.tsx
+++ components/contols.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { TimerPickerModal } from "react-native-timer-picker";
 
+type TimerType = "water" | "pesticides" | "fertilizer";
+
 const Controls = () => {
+	const [activeTimer, setActiveTimer] = useState<TimerType | null>(null);
+
 	const [waterTimer, setWaterTimer] = useState(false);
 	const [pesticidesTimer, setPesticidesTimer] = useState(false);
 	const [fertilizerTimer, setFertilizerTimer] = useState(false);
@@ -59,8 +63,19 @@ const Controls = () => {
 	}, [isRunning, timerSeconds]);
 
 	return (
-		<View className="p-2">
-			<Text className="font-medium text-slate-700">Controls</Text>
+		<View>
+			{isRunning && timerSeconds !== null && timerSeconds > 0 ? (
+				<Text
+					className={`text-3xl text-center font-bold  ${timerSeconds <= 10 ? "text-red-700" : "text-slate-700"}`}
+				>
+					{formatSeconds(timerSeconds)}
+				</Text>
+			) : (
+				<Text className="text-3xl text-center font-bold text-slate-500">
+					00:00:00
+				</Text>
+			)}
+			<Text className="font-medium text-lg text-slate-500">Controls</Text>
 			<View className="w-28 items-center rounded-lg">
 				<TimerPickerModal
 					visible={waterTimer || pesticidesTimer || fertilizerTimer}
@@ -84,42 +99,29 @@ const Controls = () => {
 					}}
 				/>
 			</View>
-			<View className=" px-4 bg-slate-300 py-5 rounded-md mb-6">
-				{isRunning && timerSeconds !== null && timerSeconds > 0 ? (
-					<View className="pb-2 items-center">
-						<Text
-							className={`text-lg font-bold  ${timerSeconds <= 10 ? "text-red-700" : "text-slate-700"}`}
-						>
-							{formatSeconds(timerSeconds)}
-						</Text>
-					</View>
-				) : (
-					<View className=" pb-2 items-center">
-						<Text className="text-lg font-bold text-slate-500">00:00:00</Text>
-					</View>
-				)}
-				<View className="flex flex-row justify-around w-full">
-					<TouchableOpacity
-						onPress={() => setWaterTimer(true)}
-						className="bg-slate-700 py-2 w-28 items-center rounded-lg"
-					>
-						<Text className="text-slate-50 font-medium">Water</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => setPesticidesTimer(true)}
-						className="bg-slate-700 py-2 w-28 items-center rounded-lg"
-					>
-						<Text className="text-slate-50 font-medium">Pesticides</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => setFertilizerTimer(true)}
-						className="bg-slate-700 py-2 w-28 items-center rounded-lg"
-					>
-						<Text className="text-slate-50 font-medium">Fertilizer</Text>
-					</TouchableOpacity>
-				</View>
+			<View className="flex flex-row justify-around w-full px-4 bg-slate-300 py-5 rounded-md mb-6">
+				<TouchableOpacity
+					onPress={() => setWaterTimer(true)}
+					className="bg-slate-700 py-2 w-28 items-center rounded-lg"
+				>
+					<Text className="text-slate-50 font-medium">Water</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => setPesticidesTimer(true)}
+					className="bg-slate-700 py-2 w-28 items-center rounded-lg"
+				>
+					<Text className="text-slate-50 font-medium">Pesticides</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => setFertilizerTimer(true)}
+					className="bg-slate-700 py-2 w-28 items-center rounded-lg"
+				>
+					<Text className="text-slate-50 font-medium">Fertilizer</Text>
+				</TouchableOpacity>
 			</View>
-			<Text className="font-medium text-slate-700 rounded-md">Monitor</Text>
+			<Text className="font-medium text-slate-500 text-lg rounded-md">
+				Monitor
+			</Text>
 			<View className="items-center w-full px-4 bg-slate-300 py-4">
 				<Text className="font-medium text-base">Temperature</Text>
 				<Text className="font-bold text-4xl">20%</Text>
