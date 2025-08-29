787985e93847382299cb86068f43d6976a6b21ad
Grian Gajila <griangajila2@gmail.com>
2025-08-29 15:40:28 +0800

    update home

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -5,12 +5,17 @@ import { Text, View } from "react-native";
 const Home = () => {
 	return (
 		<View className="flex-1 p-4">
-			<Text className="text-2xl text-left font-bold text-slate-500">
-				Welcome Back!
-			</Text>
-			<Text className="text-base font-medium text-slate-400 mb-6">
-				The sproink makes your job easier.
-			</Text>
+			<View className=" flex flex-row gap-2 border-b border-slate-500 mb-4">
+				<Text className="w-2 h-14 rounded-sm bg-slate-500"></Text>
+				<View>
+					<Text className="text-2xl text-left font-bold text-slate-500">
+						Welcome Back!
+					</Text>
+					<Text className="text-base font-medium text-slate-400 mb-6">
+						The sproink makes your job easier.
+					</Text>
+				</View>
+			</View>
 			<View className="flex-1">
 				<Controls />
 			</View>
